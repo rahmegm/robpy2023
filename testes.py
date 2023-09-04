4fb0277485a163c71d624a669a77331eec16435c
Gustavo Morandi Rahmé <gmorandirahme@gmail.com>
2023-09-04 16:45:00 -0300

    Fazendo parte das funções da parte 1

--- testes.py
+++ testes.py
@@ -1,1 +1,15 @@
 # Arquivo de testes
+
+import numpy as np
+import RobPy as rp
+
+a = rp.cria_vetor3([1, 2, 3])
+b = rp.cria_vetor3([4, 5, 6])
+
+print(a)
+print(type(a))
+print(a.shape)
+
+print(rp.produto_escalar(a, b))
+
+print(rp.norma_vetor(a))
